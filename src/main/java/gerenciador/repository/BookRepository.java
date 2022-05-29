96ec9da621e26778186e13da0057c5d04a34d9a3
JulioEvencio <julioevencio@acad.ifma.edu.br>
2022-05-28 22:18:58 -0300

    Classe resposável pela persistência dos dados do Book e seu teste unitário Junit foram implementadas

---
+++ src/main/java/gerenciador/repository/BookRepository.java
@@ -0,0 +1,104 @@
+package gerenciador.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import gerenciador.exception.DeleteBookFailedException;
+import gerenciador.exception.FindBookFailedException;
+import gerenciador.exception.SaveBookFailedException;
+import gerenciador.exception.UpdateBookFailedException;
+import gerenciador.model.Book;
+
+public class BookRepository {
+
+	public Book findById(Integer id) {
+		try (Connection con = new ConnectionFactory().getConnection()) {
+			String sql = "SELECT nome, autor FROM tb_livro WHERE id = ?;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setInt(1, id);
+
+			ResultSet rs = stmt.executeQuery();
+			Book book = null;
+
+			while (rs.next()) {
+				book = new Book(id, rs.getString("nome"), rs.getString("autor"));
+			}
+
+			stmt.close();
+
+			return book;
+		} catch (SQLException e) {
+			throw new FindBookFailedException("Erro ao recuperar o livro pelo id!");
+		}
+	}
+
+	public List<Book> findByAll() {
+		try (Connection con = new ConnectionFactory().getConnection()) {
+			String sql = "SELECT * FROM tb_livro;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+			List<Book> list = new ArrayList<>();
+
+			while (rs.next())
+				list.add(new Book(rs.getInt("id"), rs.getString("nome"), rs.getString("autor")));
+
+			stmt.close();
+
+			return list;
+		} catch (SQLException e) {
+			throw new FindBookFailedException("Erro ao recuperar todos os livros!");
+		}
+	}
+
+	public void save(Book book) {
+		try (Connection con = new ConnectionFactory().getConnection()) {
+			String sql = "INSERT INTO tb_livro (nome, autor) VALUES (?, ?);";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, book.getName());
+			stmt.setString(2, book.getAuthor());
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new SaveBookFailedException("Erro ao salvar um livro no banco de dados!");
+		}
+	}
+
+	public void update(Book book) {
+		try (Connection con = new ConnectionFactory().getConnection()) {
+			String sql = "UPDATE tb_livro SET nome = ?, autor = ? WHERE id = ?;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, book.getName());
+			stmt.setString(2, book.getAuthor());
+			stmt.setInt(3, book.getId());
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new UpdateBookFailedException("Erro ao atualizar um livro no banco de dados!");
+		}
+	}
+
+	public void delete(Integer id) {
+		try (Connection con = new ConnectionFactory().getConnection()) {
+			String sql = "DELETE FROM tb_livro WHERE id = ?;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setInt(1, id);
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new DeleteBookFailedException("Erro ao deletar um livro no banco de dados!");
+		}
+	}
+
+}
---
+++ src/test/java/gerenciador/LivroRepositoryTest.java
@@ -0,0 +1,46 @@
+package gerenciador;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import gerenciador.model.Book;
+import gerenciador.repository.BookRepository;
+
+public class LivroRepositoryTest {
+
+	@Mock
+	private BookRepository repository;
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	void deve_recuperar_um_livro_no_banco_de_dados_pelo_id() throws Exception {
+		repository.findById(2);
+	}
+
+	@Test
+	void deve_recuperar_todos_os_livro_no_banco_de_dados() throws Exception {
+		repository.findByAll();
+	}
+
+	@Test
+	void deve_salvar_um_livro_no_banco_de_dados() throws Exception {
+		repository.save(new Book(null, "Nome teste", "Autor Teste"));
+	}
+
+	@Test
+	void deve_atualizar_um_livro_no_banco_de_dados() throws Exception {
+		repository.update(new Book(5, "Nome Atualizado", "Autor Atualizado"));
+	}
+
+	@Test
+	void deve_deletar_um_livro_no_banco_de_dados() throws Exception {
+		repository.delete(1);
+	}
+
+}
